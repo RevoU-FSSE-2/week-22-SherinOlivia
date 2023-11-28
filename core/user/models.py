282cd87a15f659940337f17c456c15c54d1e068e
Sherin Olivia <SOChronicle@gmail.com>
2023-11-28 18:05:24 +0700

    feat: created user profile and user table db model

---
+++ core/user/models.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from core.user.constants import UserRole
+
+@dataclass
+class UserDomain:
+    id: int
+    username: str
+    email: str
+    password: str
+    name: str
+    city: str
+    about_me: str
+    role: UserRole
+
