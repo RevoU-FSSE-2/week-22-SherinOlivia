cbbaf671e0ee6b4a01e19216989447bb9e221ac7
Sherin Olivia <SOChronicle@gmail.com>
2023-12-01 15:42:00 +0700

    chore: moved folders to src for deployment

--- core/user/constants.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class UserRole(Enum):
-    CLIENT = "CLIENT"
-    STAFF = "STAFF"
-    ADMIN = "ADMIN"
--- infrastructure/task/modules.py
+++
@@ -1,8 +0,0 @@
-from injector import Module, singleton, Binder
-from core.task.ports import ITaskAccessor
-from infrastructure.task.adapters import TaskAccessor
-
-class TaskModule(Module):
-
-    def configure(self, binder: Binder) -> None:
-        binder.bind(ITaskAccessor, to=TaskAccessor, scope=singleton)
